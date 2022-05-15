224e63fe10a77936f4dfb156c9b5644b3636a001
Ryuk <51106560+faroukbmiled@users.noreply.github.com>
2022-05-15 23:38:51 +0100

    fix

--- funcs.c
+++ funcs.c
@@ -87,7 +87,7 @@ void swap(stack_t **stack, unsigned int number_line)
  * Return: void
  */
 void nop(__attribute__((unused)) stack_t **stack,
-			__attribute__((unused)) unsigned int line_number)
+			__attribute__((unused)) unsigned int number_line)
 {}
 
 /**
